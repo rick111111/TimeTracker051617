092406284e9f256e3c07c434c691b0d26a986459
Fangliang Xue <faxue@microsoft.com>
2017-05-16 14:39:08 -0700

    Add MVC support

--- src/Program.cs
+++ src/Program.cs
@@ -1,9 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using Microsoft.AspNetCore.Hosting;
 using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Hosting;
 
 namespace TimeTracker051617
 {
@@ -16,7 +12,6 @@ namespace TimeTracker051617
                 .UseContentRoot(Directory.GetCurrentDirectory())
                 .UseIISIntegration()
                 .UseStartup<Startup>()
-                .UseApplicationInsights()
                 .Build();
 
             host.Run();
